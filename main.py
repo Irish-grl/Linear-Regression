d77184477ce1960267bdf46435be104b6630bbf7
Jessica Thomas <97806364+Irish-grl@users.noreply.github.com>
2022-07-20 17:05:19 -0700

    Random Age Generator

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+
+class main:
+
+    np.random.seed(0)
+    num_nulls = 1000
+
+    while num_nulls != 0:
+        new_num = np.random.randint(18, 69)
+        num_nulls =-1
+        print(new_num)
+
+main()
